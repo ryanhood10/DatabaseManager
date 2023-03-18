736335fbe84faf0cc44521fc1c22e92f050a19a3
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-03-17 20:41:24 -0500

    set up test for SQL

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // const roleSchema = fs.readFileSync(roleSchemaPath, 'utf-8');
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,11 +14,18 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: 'employee_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+//testing pool
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+  
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
